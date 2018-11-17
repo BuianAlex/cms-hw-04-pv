d1be231639ab606e2fbde6a75bff79d5c8562c79
BuianAlex <a.buyan@ukr.net>
2018-11-17 11:16:38 +0200

    - body width 100% max 2000
    - first section (wellcome) 100vh
    - for arrow icon add  anchor link to about
    - add hidden nav menu with js action
    - profile-picture change gradient to border shadow
    - add  gradient to h2
    - Latest Works item add hover and link

--- app/js/main.js
+++ app/js/main.js
@@ -4,3 +4,30 @@ document.getElementById("accept-cooki").addEventListener("click", function(){
         document.getElementsByClassName("popUpCooki")[0].style.display = "none";
     });
 
+    document.getElementById("show-nav").addEventListener("click", function(){
+        let element = document.getElementById("main-nav");
+        toggliCSS(element);
+
+    });
+    let allClose = document.getElementsByClassName('close');
+    console.log(typeof allClose);
+    
+
+    
+    Array.from(allClose).forEach(function(element) {
+        element.addEventListener('click', function(){
+            this.parentNode.closest(".modal").style.display = 'none';           
+        });
+      });
+
+
+    function toggliCSS(element) {
+
+        if (element.style.display === "none") {
+            element.style.display = "block";
+        } else {
+            element.style.display = "none";
+        }
+    }   
+
+  
